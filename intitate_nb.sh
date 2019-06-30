64123a321f5ed935ec753612e232fed2d8c41476
SamSamhuns <samhunsadamant@gmail.com>
2019-06-30 11:48:06 +0400

    Add installation script

---
+++ intitate_nb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+pip install virtualenv
+python3 -m venv venv_ml
+source venv_ml/bin/activate
+pip install -r requirements.txt
+python setup.py build_ext --inplace
+ipython kernel install --user --name=titanic_ml_kernel
+jupyter notebook titanic_survival_logistic_regr.ipynb
